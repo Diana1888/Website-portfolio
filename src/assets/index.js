0c451e30f1be9509f14a6f4f3a74b8a926aed3fb
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2025-01-04 13:01:47 -0500

    Added main section

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import GithubLineIcon from 'remixicon-react/GithubLineIcon'
+import LinkedinBoxLineIcon from 'remixicon-react/LinkedinBoxLineIcon'
+
+export const heroIcons = [<GithubLineIcon />, <LinkedinBoxLineIcon />]
