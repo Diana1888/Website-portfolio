9b830dfff4aa6d8650ee66694ad5120824ed62e5
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2025-01-07 14:37:51 -0500

    Added Skills section

--- src/assets/index.js
+++ src/assets/index.js
@@ -51,3 +51,69 @@ export const experienceData = [
     ],
   },
 ];
+
+
+//Skills
+
+export const skillsData = [
+  {
+    name: 'Figma',
+    icon: '/skills/figma.png',
+  },
+  {
+    name: 'VS Code',
+    icon: '/skills/vscode.png',
+  },
+  {
+    name: 'HTML',
+    icon: '/skills/html.png',
+  },
+  {
+    name: 'CSS',
+    icon: '/skills/css.png',
+  },
+  {
+    name: 'JavaScript',
+    icon: '/skills/js.png',
+  },
+  {
+    name: 'TailwindCSS',
+    icon: '/skills/tailwind.png',
+  },
+  {
+    name: 'Vite',
+    icon: '/skills/vite.png',
+  },
+  {
+    name: 'ReactJS',
+    icon: '/skills/react.png',
+  },
+  {
+    name: 'TypeScript',
+    icon: '/skills/ts.png',
+  },
+  {
+    name: 'AI',
+    icon: '/skills/ai.png',
+  },
+  {
+    name: 'Framer Motion',
+    icon: '/skills/framer.png',
+  },
+  {
+    name: 'NextJS',
+    icon: '/skills/nextjs.png',
+  },
+  {
+    name: 'NodeJS',
+    icon: '/skills/nodejs.png',
+  },
+  {
+    name: 'MongoDB',
+    icon: '/skills/mongodb.png',
+  },
+  {
+    name: 'Github',
+    icon: '/skills/github.png',
+  },
+];
---
+++ src/components/Skills.jsx
@@ -0,0 +1,52 @@
+'use client'
+
+import Image from 'next/image'
+import Heading from './sub/Heading'
+import { skillsData } from '@/assets'
+import { delay, motion } from 'framer-motion'
+
+const Skills = () => {
+  const variants = {
+    hidden: {
+      opacity: 0,
+      y: 30,
+    },
+    visible: (i) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.3 + i * 0.07,
+      },
+    }),
+  }
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-y-20 px-96">
+      <Heading text={'Skills'} />
+      <div className="w-full flex justify-between flex-wrap gap-x-8 gap-y-10 lg:gap-y-6">
+        {skillsData.map((skill, i) => (
+          <motion.div
+            custom={i}
+            variants={variants}
+            initial="hidden"
+            whileInView="visible"
+            whileHover={{ scale: 1.1 }}
+            viewport={{ margin: '50px', once: true }}
+            key={i}
+            className="flex items-center justify-center gap-x-2 rounded-xl border border-yellow-500 bg-zinc-200 px-5 py-2 lg:px-2"
+          >
+            <Image
+              src={skill.icon}
+              alt="Skills Image"
+              width={100}
+              height={100}
+              className="h-auto w-[40px]"
+            />
+            <p className="text-sm text-gray-600">{skill.name}</p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Skills
